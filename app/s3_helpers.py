86ebf30ec7087d84ff719a5541fdec9d63fa772f
namju <namjubae@gmail.com>
2023-01-03 17:55:51 -0600

    change object name to image

--- app/s3_helpers.py
+++ app/s3_helpers.py
@@ -27,10 +27,11 @@ def get_unique_filename(filename):
 
 def upload_file_to_s3(file, acl="public-read"):
     try:
+        print("files #########", file)
         s3.upload_fileobj(
             file,
             BUCKET_NAME,
-            file.filename,
+            "image",
             ExtraArgs={
                 "ACL": acl,
                 "ContentType": file.content_type
