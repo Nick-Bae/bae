a48fac3b400a6ca2b351dff437e15f82afe42b62
namju <namjubae@gmail.com>
2023-01-06 10:17:11 -0600

    default code

--- app/api/images_routes.py
+++ app/api/images_routes.py
@@ -5,30 +5,56 @@ from flask_login import login_required, current_user
 from app.s3_helpers import (
     upload_file_to_s3, allowed_file, get_unique_filename)
 
-from ..forms import ImageForm
 
 images_routes = Blueprint('images', __name__)
 
+# @images_routes.route("", methods=["POST"])
+# @login_required
+# def upload_image( ):
+#     if "image" not in request.files:
+#         return {"errors": "image required"}, 400
+
+#     images = request.files.getlist('image')
+#     # images = request.files["image"]
+#     print("this is a receving data from front", images)
+    
+#     upload =[];
+    
+#     for image in images:
+#         if not allowed_file(image.filename):
+#             return {"errors": "file type not permitted"}, 400
+#         image.filename = get_unique_filename(image.filename)
+#         print("before uploading file#######",image)
+#         upload.append(upload_file_to_s3(image));
+
+#     return {i+1: upload[i] for i in range(len(upload))}
+# ===============================================================
 @images_routes.route("", methods=["POST"])
-@login_required
-def upload_image( ):
+def upload_image():
     if "image" not in request.files:
         return {"errors": "image required"}, 400
 
-    images = request.files.getlist('image')
-    # images = request.files["image"]
-    print("this is a receving data from front", images)
-    
-    upload =[];
-    
-    for image in images:
-        if not allowed_file(image.filename):
-            return {"errors": "file type not permitted"}, 400
-        image.filename = get_unique_filename(image.filename)
-        print("before uploading file#######",image)
-        upload.append(upload_file_to_s3(image));
+    image = request.files["image"]
 
-    return {i+1: upload[i] for i in range(len(upload))}
+    if not allowed_file(image.filename):
+        return {"errors": "file type not permitted"}, 400
+    
+    image.filename = get_unique_filename(image.filename)
+
+    upload = upload_file_to_s3(image)
+
+    if "url" not in upload:
+        # if the dictionary doesn't have a url key
+        # it means that there was an error when we tried to upload
+        # so we send back that error message
+        return upload, 400
+
+    url = upload["url"]
+    # flask_login allows us to get the current user from the request
+    # new_image = Image(user=current_user, url=url)
+    # db.session.add(new_image)
+    # db.session.commit()
+    return {"url": url}
 
 
 def validation_errors_to_error_messages(validation_errors):
